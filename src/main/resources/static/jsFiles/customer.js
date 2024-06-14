84456dc826999b265a444bcb0fa5320c13387018
Angad Kantilala Raut <angraut@altimetrik.com>
2024-06-14 17:39:14 +0530

    front-end files deleted

--- src/main/resources/static/jsFiles/customer.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    if (localStorage.getItem('userName')!=null && localStorage.getItem('isAdmin')!=null && localStorage.getItem('userMobile')!=null) {
-
-    } else {
-
-    }
-});
